ca87776a284de29f6d2a041ecda7f4e6155e0c0e
Saúl <1725110308@utectulancingo.edu.mx>
2026-07-09 14:39:56 +0000

    FI tabla mal cerrada

--- contactos/app.py
+++ contactos/app.py
@@ -2,7 +2,6 @@ import web
 urls = (
     '/','controllers.index.Index',
     '/borrar_contacto/(.*)','controllers.borrar_contacto.Borrar_contacto',
-    '/','controllers.insertar_contacto.Insertar_contacto',
     '/lista_contactos','controllers.lista_contactos.ListaContactos',
     '/modificar_contacto/(.*)','controllers.modificar_contacto.Modificar_contacto',
     '/ver_contacto/(.*)','controllers.ver_contactos.Ver_contactos',
